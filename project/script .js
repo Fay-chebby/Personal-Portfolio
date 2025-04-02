11e81eec540ed387cde73d38f9067f28434f5cd6
Fay-chebby <chebetf034@gmail.com>
2025-04-03 00:22:55 +0300

    skills page

--- project/script .js
+++ project/script .js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".text-section.left").classList.add("show");
+  document.querySelector(".chart-container").classList.add("show");
+  document.querySelector(".text-section.right").classList.add("show");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    document.querySelector(".text-section.left").classList.add("show");
+    document.querySelectorAll(".bar").forEach((bar) => {
+      bar.classList.add("animated");
+    });
   }, 500);
-  setTimeout(() => {
-    document.querySelector(".chart-container").classList.add("show");
-  }, 1000);
-  setTimeout(() => {
-    document.querySelector(".text-section.right").classList.add("show");
-  }, 1500);
 });
